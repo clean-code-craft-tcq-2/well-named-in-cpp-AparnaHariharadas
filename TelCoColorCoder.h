2cdc6a655527a15ff79e42f5ed8c5bbec459a71a
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-10 11:45:26 +0530

    Update TelCoColorCoder.h

--- TelCoColorCoder.h
+++ TelCoColorCoder.h
@@ -39,3 +39,6 @@ namespace TelCoColorCoder
                 return colorPairStr;
             }
     };
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+}
