655729de8ce833b2e428e00fbdc03339b0040888
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-10 16:01:28 +0530

    Update TelCoColorCoder.cpp

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -15,7 +15,7 @@ int TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::MajorColor major, T
 void TelCoColorCoder::PrintColorCodeManual(){
         int initial_index_major,initial_index_minor,color_index = 0;
         int major_color,minor_color;
-        std::cout << " Major_colour,  minor_colour, value" << std::endl;
+        std::cout << TelCoColorCoder::numberOfMajorColors << std::endl;
                 for (major_color = initial_index_major;major_color<TelCoColorCoder::numberOfMajorColors;major_color++){
                         for (minor_color = initial_index_minor;minor_color<TelCoColorCoder::numberOfMinorColors;minor_color++){
                        
