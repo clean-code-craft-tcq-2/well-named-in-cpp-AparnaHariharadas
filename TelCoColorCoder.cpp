5e2dd9bbdea694cb299ad7fcff82cf890ae4e15c
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-10 11:58:25 +0530

    Update TelCoColorCoder.cpp

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "TelCoColorCoder.h"
 
-    TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
+    TelCoColorCoder::ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         TelCoColorCoder::MajorColor majorColor = 
             (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
@@ -10,7 +10,7 @@
             (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % TelCoColorCoder::numberOfMinorColors);
         return TelCoColorCoder::ColorPair(majorColor, minorColor);
     }
-    int GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
+    int TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
         return major * TelCoColorCoder::numberOfMinorColors + minor + 1;
     }
 
