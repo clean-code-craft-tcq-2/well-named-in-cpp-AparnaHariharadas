30ce45c5226949e39ad14881050b3c596027a7dd
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-17 19:27:25 +0530

    Update TestTelCoColorCoder.cpp

--- TestTelCoColorCoder.cpp
+++ TestTelCoColorCoder.cpp
@@ -1,4 +1,4 @@
-#include "TestTelCoColorCoder.h"
+#include "TelCoColorCoder.h"
 
 void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor){
     TelCoColorCoder::ColorPair colorPair =
@@ -16,4 +16,13 @@ void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorC
 void test_colorcodeManual(){
     std::cout << "Colour 1 " << "Colour 2 " << "Value"<< std::endl;
     TelCoColorCoder::PrintColorCodeManual();
+}
+int main() {
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    test_colorcodeManual();
+    return 0;
 }
