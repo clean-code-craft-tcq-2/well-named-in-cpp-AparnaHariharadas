d7426e076e2210450ae20343a053302deced5130
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-17 19:01:58 +0530

    Update TelCoColorCoder.cpp

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -1,10 +1,15 @@
 #include "TelCoColorCoder.h"
 
-
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
     int numberOfMajorColors =
-        sizeof(TelCoColorCoder::MajorColorNames) / sizeof(TelCoColorCoder::MajorColorNames[0]);
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
     int numberOfMinorColors =
-        sizeof(TelCoColorCoder::MinorColorNames) / sizeof(TelCoColorCoder::MinorColorNames[0]);
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 TelCoColorCoder::ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
@@ -21,7 +26,7 @@ void TelCoColorCoder::PrintColorCodeManual(){
         int major_color,minor_color;
         for (major_color = 0;major_color<numberOfMajorColors;major_color++){
                 for (minor_color = 0;minor_color<numberOfMinorColors;minor_color++){                       
-                       std::cout << TelCoColorCoder::MajorColorNames[major_color] <<" " << TelCoColorCoder::MinorColorNames[minor_color] << " " << color_index << std::endl;
+                       std::cout << MajorColorNames[major_color] <<" " << MinorColorNames[minor_color] << " " << color_index << std::endl;
                        color_index ++;
                }
         }
