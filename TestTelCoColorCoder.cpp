ca2b4f88cb7ddbe92e9f309012d438add2a93fd8
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-17 19:06:10 +0530

    Update TestTelCoColorCoder.cpp

--- TestTelCoColorCoder.cpp
+++ TestTelCoColorCoder.cpp
@@ -17,3 +17,13 @@ void test_colorcodeManual(){
     std::cout << "Colour 1 " << "Colour 2 " << "Value"<< std::endl;
     TelCoColorCoder::PrintColorCodeManual();
 }
+
+int main() {
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    test_colorcodeManual();
+    return 0;
+}
