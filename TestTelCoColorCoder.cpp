2bfe76bc2bf3ce7440c711512c1f1f3ee88548dc
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-17 17:51:51 +0530

    Update TestTelCoColorCoder.cpp

--- TestTelCoColorCoder.cpp
+++ TestTelCoColorCoder.cpp
@@ -2,18 +2,18 @@
 #include "TelCoColorCoder.cpp"
 void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor){
     TelCoColorCoder::ColorPair colorPair =
-        GetColorFromPairNumber(pairNumber);
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
 }
 
 void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber){
-    int pairNumber = GetPairNumberFromColor(major, minor);
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
 void test_colorcodeManual(){
     std::cout << "Colour 1 " << "Colour 2 " << "Value"<< std::endl;
-    PrintColorCodeManual();
+    TelCoColorCoder::PrintColorCodeManual();
 }
