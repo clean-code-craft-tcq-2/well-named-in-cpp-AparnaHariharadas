671ba687ccbeb71c9cc723eafe63844bc89f798e
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-17 17:51:27 +0530

    Update TelCoColorCoder.cpp

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -1,16 +1,16 @@
 #include "TelCoColorCoder.h"
 
-TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
+TelCoColorCoder::ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         TelCoColorCoder::MajorColor majorColor = (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
        TelCoColorCoder::MinorColor minorColor = (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % TelCoColorCoder::numberOfMinorColors);
         return TelCoColorCoder::ColorPair(majorColor, minorColor);
     }
-int GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
+int TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
         return major * TelCoColorCoder::numberOfMinorColors + minor + 1;
     }
 
-void PrintColorCodeManual(){
+void TelCoColorCoder::PrintColorCodeManual(){
         int color_index = 1;
         int major_color,minor_color;
         for (major_color = 0;major_color<TelCoColorCoder::numberOfMajorColors;major_color++){
